47959f0c427fe13266283666a0e7e1d072e6fc7f
lautter <lautter@ufrn.edu.br>
2015-10-15 00:28:51 -0300

    Update header.hpp

--- src/header.hpp
+++ src/header.hpp
@@ -1,7 +1,15 @@
 #ifndef BIBLIOTECA_HPP
 #define BIBLIOTECA_HPP
 #include <iostream>
-
+ /**
+  * @brief Classe Pilha e seu construtor
+  * A função construtora recebe o argumento _MaxSz a fim de definir o tamanho máximo da pilha.
+  * Depois testa se a pilha foi criada e inicializa seus elementos.
+  * @param _MaxSz tamanho maximo da pilha
+  * @param pt_fila ponteiro para a pilha
+  * @param tamanho tamanho da pilha, é incrementado ou decrementado de acordo com as ações.
+  * @param capacidade capacidade máxima da pilha
+  */
 template<typename P>
 class Pilha{
 	protected:
@@ -9,16 +17,61 @@ class Pilha{
 		int tamanho;
 		int _MaxSz;
 		int capacidade;
+ /**
+ * @brief _duplica() Duplica a pilha caso tenha alcançado a capacidade máxima
+ * 
+ * @param *aux ponteiro auxiliar que vai receber a pilha temporariamente
+ * @param *novo ponteiro para um vetor do tipo pilha com o dobro da capacidade anterior
+ * o laço for copia o vetor aux para o "novo" que em seguida em copiado para
+ * @see pt_Pilha
+ * @return true or false
+ * */
 		bool _duplica(void);
 	public:
 		Pilha(int _MaxSz=50);
+  /**
+  * @brief Destructor da pilha.
+  * Destrói a pilha.
+  */
 		~Pilha(void);
-
+ /**
+ *@brief pop() remove o último elemento da pilha somente decrementando seu tamanho
+ * 
+ * @see pt_Pilha
+ * @see empty()
+ * @see tamanho
+ **/
 		bool pop();
+ /**
+ * @brief push(P _data) insere o elemento _data do tipo P na pilha
+ * @param _data 
+ * @see pt_Pilha
+ * @see tamanho
+ * @return true or false
+ * */
 		bool push(P _data);
+ /**
+ * top() retorna o último elemento da pilha (tamanho-1)
+ * @see pt_Pilha
+ * @see tamanho
+ * @return pt_Pilha[tamanho-1]
+ * */
 		P top();
+ /**
+ * @brief size() retorna o tamanho atual da pilha
+ * caso não seja inválida 
+ **/
 		int size() const;
+ /**
+ * @brief Full() retorna true se o tamanho do vetor tiver alcançado o tamanho máximo
+ * @see tamanho 
+ * @see _MaxSz
+ * */
 		bool Full() const;
+ /**
+ * @brief empty() retorna true se a pilha estiver vazia
+ * @see tamanho
+ * */
 		bool empty() const;
 
 
@@ -31,6 +84,16 @@ class Pilha{
 			return _os;
 		}
 };
+ /**
+  * @brief classe fila e seu construtor com o tipo genérico F
+  * A função construtora recebe o argumento _MaxSz a fim de definir o tamanho máximo da fila.
+  * Depois testa se a fila foi criada e inicializa seus elementos.
+  * @param _MaxSz tamanho maximo da fila
+  * @param pt_fila ponteiro para a fila
+  * @param tamanho tamanho da fila, é incrementado ou decrementado de acordo com as ações.
+  * @param inicio o primeiro elemento da fila
+  * @param fim o último elemento da fila.
+  */
 template<typename F>
 class Fila{
 	protected:
@@ -41,16 +104,64 @@ class Fila{
 		int tamanho;
 	public:
 		Fila(int _MaxSz=50);
+  /**
+  * @brief ~Fila() Destructor da fila. 
+  * 
+  * Destrói a fila.
+  */
 		~Fila(void);
-
-		F front() const;
-		F back() const;
-        bool pop();
-		bool push(F _data);
+  /**
+  * front retorna o primeiro elemento da fila.
+  * @see pt_fila
+  * @see inicio
+  * @return pt_fila[inicio]
+  */		F front() const;
+  /**
+  * back retorna o último elemento da fila. 
+  * @see pt_fila
+  * @see fim
+  * @return pt_fila[fim]
+  */		F back() const;
+   /**
+  * @brief "pop" tem por objetivo remover um elemento da fila.
+  * 
+  * Primeiro a função avalia se a fila foi criada/é válida para então remover o primeiro elemento da fila e
+  * decrementar o tamanho. Retorna true caso dê certo.
+  * @see pt_fila
+  * @see empty()
+  * @see inicio
+  * @see tamanho
+  * @return true or false
+  */        	bool pop();
+   /**
+  * @brief Push tem por objetivo inserir um elemento na fila.
+  * 
+  * "push" recebe um argumento _data e, caso a fila seja válida e não estiver cheia, testa se está vazia
+  * para então inserir o elemento e incrementar o tamanho da fila.
+  * @see pt_fila ponteiro para o primeiro elemento da fila
+  * @see full()
+  * @see inicio
+  * @see fim
+  * @see tamanho
+  */		bool push(F _data);
+   /**
+  * A função full retorna verdadeiro se a fila estiver cheia e falso caso não.
+  * @return true or false
+  */
 		bool full() const;
+  /**
+  * empty() retorna true se fila estiver vazia.
+  * @return true or false
+  */
 		bool empty() const;
-        int size() const;
-
+  /**
+  * A função size retorna o tamanho da fila.
+  * @return this->tamanho
+  */
+        	int size() const;
+  /*
+  * sobrecarga do operador <<
+  **/
         inline friend
 		std::ostream &operator <<(std::ostream &_os,const Fila &_obj){
 			_os<<"[";
