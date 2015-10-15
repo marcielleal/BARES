c709c16da8f165f0548b8d2f0f51ceb0bff3f4f5
lautter <lautter@ufrn.edu.br>
2015-10-15 00:28:46 -0300

    Update Pilha.cpp

--- src/Pilha.cpp
+++ src/Pilha.cpp
@@ -1,19 +1,5 @@
 #include <iostream>
 #include "header.hpp"
-/**
- *  @brief O objetivo é definir o tipo de dados da classe pilha
- *  e a implementação de suas funções. 
- */
- 
- /**
-  * @brief Constructor da pilha. 
-  * A função construtora recebe o argumento _MaxSz a fim de definir o tamanho máximo da pilha.
-  * Depois testa se a pilha foi criada e inicializa seus elementos.
-  * @param _MaxSz tamanho maximo da pilha
-  * @param pt_fila ponteiro para a pilha
-  * @param tamanho tamanho da pilha, é incrementado ou decrementado de acordo com as ações.
-  * @param capacidade capacidade máxima da pilha
-  */
 
 template<typename P>
 Pilha<P>::Pilha(int _MaxSz){
@@ -23,26 +9,12 @@ Pilha<P>::Pilha(int _MaxSz){
 		this->tamanho=0;
 	}
 }
- /**
-  * @brief Destructor da pilha. 
-  * 
-  * Destrói a pilha.
-  */
+
 template<typename P>
 Pilha<P>::~Pilha(){
 	delete [] this->pt_Pilha;
 }
 
-/**
- * @brief _duplica() Duplica a pilha caso tenha alcançado a capacidade máxima
- * 
- * @param *aux ponteiro auxiliar que vai receber a pilha temporariamente
- * @param *novo ponteiro para um vetor do tipo pilha com o dobro da capacidade anterior
- * o laço for copia o vetor aux para o "novo" que em seguida em copiado para
- * @see pt_Pilha
- * @return true or false
- * 
- * */
 template<typename P>
 bool Pilha<P>::_duplica(){
 	P *aux=this->pt_Pilha;
@@ -54,10 +26,7 @@ bool Pilha<P>::_duplica(){
 	delete [] aux;
 	return true;
 }
-/**
- * @brief size() retorna o tamanho atual da pilha
- * caso não seja inválida 
- **/
+
 template<typename P>
 int Pilha<P>::size() const
 {
@@ -65,34 +34,20 @@ int Pilha<P>::size() const
 
 	return this->tamanho;
 }
-/**
- * @brief Full() retorna true se o tamanho do vetor tiver alcançado o tamanho máximo
- * @see tamanho 
- * @see _MaxSz
- * */
+
 template<typename P>
 bool Pilha<P>::Full() const
 {
 	if(pt_Pilha==NULL) return true;
 	return (this->tamanho==this->_MaxSz);
 }
-/**
- * @brief empty() retorna true se a pilha estiver vazia
- * @see tamanho
- * */
+
 template <typename P>
 bool Pilha<P>::empty() const
 {
 	return this->tamanho==0;
 }
-/**
- * @brief push(P _data) insere o elemento _data do tipo P na pilha
- * @param _data 
- * @see pt_Pilha
- * @see tamanho
- * @return true or false
- * 
- * */
+
 template <typename P>
 bool Pilha<P>::push(P _data)  // insere
 {
@@ -102,15 +57,8 @@ bool Pilha<P>::push(P _data)  // insere
 	return true;
 }
 
-/**
- *@brief pop() remove o último elemento da pilha somente decrementando seu tamanho
- * 
- * @see pt_Pilha
- * @see empty()
- * @see tamanho
- **/
 template <typename P>
-bool Pilha<P>::pop() //remove
+bool Pilha<P>::pop()
 {
 	if(pt_Pilha == NULL || this->empty())
 		return false;
@@ -118,12 +66,6 @@ bool Pilha<P>::pop() //remove
 	return true;
 }
 
-/**
- * top() retorna o último elemento da pilha (tamanho-1)
- * @see pt_Pilha
- * @see tamanho
- * @return pt_Pilha[tamanho-1]
- * */
 template <typename P>
 P Pilha<P>::top()
 {
